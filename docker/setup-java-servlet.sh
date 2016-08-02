bf9c8b00cb30b244565a24738a1f33a75ae12699
maehaschi08 <maehachi08@gmail.com>
2016-08-02 03:24:16 -0400

    apache + tomcatが動作するdockerコンテナをDockerfileからビルドする一式
    コミットするファイルについて
    ---
    1. docker/Dockerfile
      - dockerコンテナイメージをビルドするための設定ファイル
      - CMDは1つのみ指定できるためsupervisordを起動(httpdやtomcatはsupervisordに起動してもらう)
    
    2. docker/hello.tgz
      - Java Servletで動作するHelloWorldプログラム
    
    3. docker/setup-java-servlet.sh
      - apacheとtomcatをインストールするシェルスクリプト
    
    4. docker/supervisord.conf
      - プロセス管理ツールのsupervisordの設定ファイル
    
    5. docker/apache_tomcat.ini
      - プロセス管理ツールのsupervisordのapacheとtomcatを監視し、プロセスが起動していない場合は起動するための監視設定ファイル
    
    6. docker/tomcat.conf
      - apacheで受けたリクエストをtomcatへプロキシするための設定ファイル
    
    本ディレクトリでdockerビルドとコンテナ起動
    ---
    docker build -t maehachi08/java-hello-world ./
    docker run --privileged -d -p 80:80 --name test maehachi08/java-hello-world:latest /sbin/init

---
+++ docker/setup-java-servlet.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# apache + tomcatインストール
+
+# epel
+rpm -ivh http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-7.noarch.rpm
+
+# apache + tomcat でJava Servletを動かす
+
+## 事前準備
+systemctl stop firewalld
+systemctl disable firewalld
+
+## インストール
+### java実行環境をインストール
+yum install -y java-1.8.0-openjdk 
+yum install -y java-1.8.0-openjdk-headless
+yum install -y java-1.8.0-openjdk-devel
+
+### apacheをインストール
+yum install -y httpd
+
+### tomcatをインストール
+yum install -y tomcat
+
+### サービス自動起動を有効化
+systemctl enable httpd
+systemctl enable tomcat
