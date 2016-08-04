c551ddcc80cea7db1a0906721e08d5139952c9ce
maehachi08 <maehachi08@gmail.com>
2016-08-04 17:58:48 +0900

    docker buildを実行し、作成したdockerマシンイメージをECRレポジトリへpushするスクリプト

---
+++ docker/java_servlet_hello_world-docker-build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# 
+# Name:
+#   java_servlet_hello_world-docker-build.sh
+# 
+# Description:
+#  Java Servlet Hello Worldアプリケーションが動作するdockerコンテナイメージをビルドする
+#
+
+# dockerコンテナイメージをビルド
+docker build -t maehachi08/java-hello-world /tmp/docker
+
+# ECRログイン
+aws ecr get-login --region us-east-1 | sh
+export AWS_DEFAULT_REGION='us-east-1'
+
+# ECR用のタグ付け
+docker tag maehachi08/java-hello-world:latest 375144106126.dkr.ecr.us-east-1.amazonaws.com/ecr-handson-httpd:latest
+
+# ECRへpush
+docker push 375144106126.dkr.ecr.us-east-1.amazonaws.com/ecr-handson-httpd:latest
