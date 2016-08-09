dffd655f04f5ec1ba38ec2129b019b3d1c540d2b
maehachi08 <maehachi08@gmail.com>
2016-08-09 16:36:09 +0900

    fix epel install

--- docker/setup-java-servlet.sh
+++ docker/setup-java-servlet.sh
@@ -2,7 +2,7 @@
 # apache + tomcatインストール
 
 # epel
-rpm -ivh http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-7.noarch.rpm
+yum install -y epel-release
 yum install -y supervisor
 
 # apache + tomcat でJava Servletを動かす
