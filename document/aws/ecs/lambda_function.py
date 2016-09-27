5a855ead13035973d440121c434d7b2f25a5ac69
maehachi08 <maehachi08@gmail.com>
2016-09-27 09:14:29 +0000

    ECS Optimized AMI のAMI-IDを取得するPythonスクリプト

---
+++ document/aws/ecs/lambda_function.py
@@ -0,0 +1,53 @@
+# coding:utf-8
+#  Name:
+#    get-ecs-ami.py
+#
+#  Description:
+#    ECS Optimized AMIのAMI IDをAWSドキュメント内から取得する
+#
+#    AWS Lambdaで使用する場合は
+#    BeautifulSoupライブラリとchardetライブラリをデプロイパッケージに含めること
+#
+#    refs https://docs.aws.amazon.com/ja_jp/lambda/latest/dg/lambda-python-how-to-create-deployment-package.html
+#
+import urllib2
+import chardet
+from BeautifulSoup import BeautifulSoup
+
+def lambda_handler(event, context):
+  # URLをセット
+  url = 'http://docs.aws.amazon.com/ja_jp/AmazonECS/latest/developerguide/ecs-optimized_AMI.html'
+
+  # URLを開く
+  response = urllib2.urlopen(url)
+
+  # HTMLを読み込む
+  html = response.read().decode("utf-8", "replace")
+
+  # BeautifulSoupオブジェクト
+  soup = BeautifulSoup(html)
+  table = soup.findAll( "div", { "class" : "informaltable-contents" } )[0]
+  t_body = table.findAll( "table" )[0].findAll( "tbody" )[0]
+  
+  # ディクショナリ型変数を初期化
+  dict = {}
+
+  # <code class="literal"></code> を抽出
+  #   1つ目がリージョン、2つ目がAMI IDの組みなので、
+  #   ディクショナリ型変数に格納する
+  for t_code in t_body.findAll( "tr" ):
+    k, v = BeautifulSoup( str( t_code ) ).findAll( "code", { "class" : "literal" } )
+    # k,vはunicode型インスタンスなのでstrで文字列へ変換する
+    dict[str( k.text )] = str( v.text )
+
+  for key, value in dict.items():
+    print( key,  value )
+
+  return dict
+
+#def main():
+#  lambda_handler()
+#
+#if __name__ == "__main__":
+#  main()
+
